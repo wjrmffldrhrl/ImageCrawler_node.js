71fbaa9bd9d1d5e687d23df3306c0722cb77394d
wjrmffldrhrl <wjrmffldrhrl@gmail.com>
2020-08-06 17:38:56 +0900

    check api

--- imageDownloader/imgDownloader.js
+++ imageDownloader/imgDownloader.js
@@ -1,28 +1,28 @@
-exports.downloadImgs = function(items,userId,callback){
+exports.downloadImgs = function (items, userId, callback) {
   const fs = require('fs');
   const request = require('request');
 
 
   var fileCount = 0;
-  var dir = "./contents/"+userId;
+  var dir = "./contents/" + userId;
 
   //디렉터리가 없다면 생성
-  if(!fs.existsSync(dir))
-  fs.mkdirSync(dir)
+  if (!fs.existsSync(dir))
+    fs.mkdirSync(dir)
 
 
-  items.forEach(element => {
-    var filename = dir+"/img"+fileCount+".jpg";
+  items.forEach( element => {
+    var filename = dir + "/img" + fileCount + ".jpg";
     fileCount++;
-    request.head(element,function(err,res,body){
+    request.head(element, function (err, res, body) {
       console.log('content-type:', res.headers['content-type']);
       console.log('content-length:', res.headers['content-length']);
-      request(element).pipe(fs.createWriteStream(filename)).on('close',function() {
-        console.log("download "+filename+" done"); 
+      request(element).pipe(fs.createWriteStream(filename)).on('close', () => {
+        console.log("download " + filename + " done");
         callback(filename);
       });
     });
   });
 
 
- };
+};
